c74650f69540a03d3df70fe8f0c6ba6d8de6aa41
Zach Weber <weberzt@gmail.com>
2019-12-30 23:50:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  
-  data.each{|details, value|
-    vaue.each{|description, names|
-      names.each{|name|
-        if pigeon_list[name] == nil 
-          pigeon_list[name] = {}
-        end
-        if pigeon_list[name][detials] == nil 
-          pigeon_list[name][details] = []
-        end
-        pigeon_list[name][:details].push(description.to_s)
-      }
-    }
-  }
-  pigeon_list
+  result = data.reduce({}) do |memo, (category, value_hash)|
+     value_hash.each_pair do |entry, name_array|
+       name_array.each do |name|
+         if memo[name] == nil 
+           memo[name] = {}
+         end 
+         if memo[name][category] == nil 
+           memo[name][category] = []
+         end 
+         memo[name][category].push(entry.to_s)
+       end 
+     end
+     memo
+   end 
+   p result
 end
+
+
