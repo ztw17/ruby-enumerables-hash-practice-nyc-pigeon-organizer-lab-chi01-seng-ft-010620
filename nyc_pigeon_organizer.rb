afae6dfd282fdcdd47ab0b51a8212c9f51705006
Zach Weber <weberzt@gmail.com>
2019-12-30 23:51:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,4 +16,22 @@ def nyc_pigeon_organizer(data)
    p result
 end
 
-
+pidgeons = {
+         :color => {
+           :purple => ["Theo", "Peter Jr.", "Lucky"],
+           :grey => ["Theo", "Peter Jr.", "Ms. K"],
+           :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+           :brown => ["Queenie", "Alex"]
+         },
+         :gender => {
+           :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+           :female => ["Queenie", "Ms. K"]
+         },
+         :lives => {
+           "Subway" => ["Theo", "Queenie"],
+           "Central Park" => ["Alex", "Ms. K", "Lucky"],
+           "Library" => ["Peter Jr."],
+           "City Hall" => ["Andrew"]
+         }
+ }
+ nyc_pigeon_organizer(pidgeons)
