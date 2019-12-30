288c0a32314f88bc6c26571447cbc659062a4804
Zach Weber <weberzt@gmail.com>
2019-12-30 23:48:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each{|details, value|
+    vaue.each{|description, names|
+      names.each{|name|
+        if pigeon_list[name] == nil 
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][detials] == nil 
+          pigeon_list[name][details] = []
+        end
+        pigeon_list[name][:details].push)description
 end
